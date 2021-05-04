eb7241f62dac07ec98127355b9c14a5f6cb28623
Charlton Smith <csmith12@amfam.com>
2021-05-04 13:02:55 -0400

    first commit

---
+++ app/src/main/java/nyc/charlton/scw/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package nyc.charlton.scw.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
---
+++ app/src/main/java/nyc/charlton/scw/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package nyc.charlton.scw.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
